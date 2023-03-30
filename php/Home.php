0f33d82e059e17a22f065dbc7249cea3f582c3f5
Dikesh Khatri <np03cs4s220023@heraldcollege.edu.np>
2023-03-30 11:48:28 +0545

    Linked css for issue#1

--- php/Home.php
+++ php/Home.php
@@ -7,7 +7,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-   <link rel="stylesheet" href="\css\home.php">
+   <link rel="stylesheet" href="/GroupE_G6/css/home.css">
 </head>
 
 <body>
