24da61aa06446acd3f3de436d3edce3d43911ebc
Alash Shrestha <np03cs4s220081@heraldcollege.edu.np>
2023-03-30 11:33:18 +0545

    Fixed map display for issue#2

--- php/Home.php
+++ php/Home.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
 
-   <head>
-      <meta charset="utf-8">
-      <title>Responsive Navigation Menu</title>
-      <!-- <link rel="stylesheet" href="style.css"> -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-      <link rel="stylesheet" href="\css\home.php">
-   </head>
-   
+<head>
+   <meta charset="utf-8">
+   <title>Responsive Navigation Menu</title>
+   <!-- <link rel="stylesheet" href="style.css"> -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+   <link rel="stylesheet" href="\css\home.php">
+</head>
 
+<body>
+   <nav>
 
-   <body>
-      <nav>
-        
-         <div class="logo">
-          TMS
-         </div>
-        
-         <input type="checkbox" id="click">
-         <label for="click" class="menu-btn">
+      <div class="logo">
+         TMS
+      </div>
+
+      <input type="checkbox" id="click">
+      <label for="click" class="menu-btn">
          <i class="fas fa-bars"></i>
-         </label>
-         <ul>
-            <li><a class="active" href="#">Home</a></li>
-            <li><a href="#">Account</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Help</a></li>
-         </ul>
-      </nav>
-      <!-- <div id="map" class="container-fluid"></div> -->
-   </body>
+      </label>
+      <ul>
+         <li><a class="active" href="#">Home</a></li>
+         <li><a href="#">Account</a></li>
+         <li><a href="loginpage.php">Settings</a></li>
+         <li><a href="#">About us</a></li>
+         <li><a href="#">Help</a></li>
+      </ul>
+   </nav>
+   <div id="map" class="container-fluid content"></div>
+</body>
 <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-<script src="map.js"></script>
+<script src="/GroupE_G6/js/map.js"></script>
+
 </html>
